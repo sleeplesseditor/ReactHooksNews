9f4c2b1b999af167f221228e94a31e26e68def6c
Josh Bryson <josh@bryson.me.uk>
2019-05-21 00:10:02 +0100

    Axios connection to HackerNews API established

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+      .then(response => {
+        console.log(response.data);
+        setResults(response.data.hits);
+      })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <ul>
+        {results.map(result => (
+          <li key={result.objectID}>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
-
-export default App;
