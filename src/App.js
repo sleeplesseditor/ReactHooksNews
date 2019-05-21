e4bf0e27703cb806b7abcf230b91f727895fe9fa
Josh Bryson <josh@bryson.me.uk>
2019-05-21 20:18:58 +0100

    Minor styling update and point display added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import './App.css';
 
 export default function App() {
   const [results, setResults] = useState([]);
@@ -20,6 +19,7 @@ export default function App() {
       const response = await axios
         .get(`http://hn.algolia.com/api/v1/search?query=${query}`);
       setResults(response.data.hits); 
+      console.log(response.data.hits);
     } catch(err) {
       setError(err)
     }
@@ -37,13 +37,13 @@ export default function App() {
   }
 
   return (
-    <div className="container max-w-md mx-auto p-4 m-2 bg-gray-100 rounded">
+    <div className="container w-1/2 mx-auto p-4 bg-gray-100 rounded">
       <img 
         src="https://icon.now.sh/react/c0c" 
         alt="React Logo" 
         className="float-right h-12"
       />
-      <h1 className="text-gray-darkest font-thin">
+      <h1 className="text-3xl">
         Hooks News
       </h1>
       <form 
@@ -59,14 +59,14 @@ export default function App() {
         />
         <button 
           type="submit"
-          className="bg-blue-500 m-1 p-1 text-white rounded"
+          className="bg-blue-500 m-1 p-1 text-white rounded hover:bg-blue-400"
         >
           Search
         </button>
         <button 
           type="button"
           onClick={handleClearSearch}
-          className="bg-red-600 text-white p-1 rounded"
+          className="bg-red-600 text-white p-1 rounded hover:bg-red-700"
         >
           Clear
         </button>
@@ -80,10 +80,11 @@ export default function App() {
           {results.map(result => (
             <li key={result.objectID}>
               <a 
-                className="text-darkest-gray hover:text-light-gray"
+                className="text-darkest-gray hover:text-gray-500 leading-loose"
                 href={result.url}>
                   {result.title}
               </a>
+              <p className="float-right">{result.points} pts</p>
             </li>
           ))}
         </ul>
