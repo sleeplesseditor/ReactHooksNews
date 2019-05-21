caac5196e5f1fcc2f74b293be067fc99b4659fcf
Josh Bryson <josh@bryson.me.uk>
2019-05-21 08:35:14 +0100

    Basic search functionality added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,36 @@ import axios from 'axios';
 import './App.css';
 
 export default function App() {
-  const [results, setResults] = useState([])
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('react hooks')
 
   useEffect(() => {
-    axios
-      .get('http://hn.algolia.com/api/v1/search?query=reacthooks')
-      .then(response => {
-        console.log(response.data);
-        setResults(response.data.hits);
-      })
+    getResults();
   }, []);
+
+  const getResults = async () => {
+    const response = await axios
+      .get(`http://hn.algolia.com/api/v1/search?query=${query}`);
+    setResults(response.data.hits); 
+  }
+
+  const handleSearch = event => {
+    event.preventDefault();
+    getResults();
+  }
 
   return (
     <Fragment>
+      <form onSubmit={handleSearch}>
+        <input 
+          type="text" 
+          onChange={event => setQuery(event.target.value)}
+          value={query} 
+        />
+        <button type="submit">
+          Search
+        </button>
+      </form>
       <ul>
         {results.map(result => (
           <li key={result.objectID}>
